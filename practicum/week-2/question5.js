bc992e0794e3cc7755b532eff2c9a4db3fc41dc5
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-02-06 22:43:26 -0700

    Try Again

--- practicum/week-2/question5.js
+++ practicum/week-2/question5.js
@@ -12,7 +12,14 @@ output: 3
 */
 
 var minimalReduction = function(n) {
-    
-
+    if (n == 0){
+        return 0;
+    } else if (n % 3 == 0){
+        return 1 + minimalReduction(n/3);
+    } else if (n % 4 == 0){
+        return 1 + minimalReduction(n/4);
+    } else if (n - 1 == 0){
+        return 1 + minimalReduction(n-1)
+    }
 };
 
