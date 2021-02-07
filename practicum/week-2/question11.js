4500d7e19ac59071ef0c2947a73ce25d9f91c88f
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-02-06 21:15:23 -0700

    Commit

--- practicum/week-2/question11.js
+++ practicum/week-2/question11.js
@@ -10,5 +10,12 @@ output: [5, 5, 2, 2, 2, 2]
 */
 
 var decode = function(input) {
+    let array = [];
 
+    for(var i = 0; i < input.length; i += 2) {
+        for(var j = 0; j < input[i]; j++) {
+            array.push(input[i+1]);
+        }
+    }
+    return array;
 };
