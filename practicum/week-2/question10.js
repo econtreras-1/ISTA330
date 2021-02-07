038c7f9d0196ec50e8e0a87a8572b0fd151fe3b4
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-02-06 20:57:17 -0700

    test

--- practicum/week-2/question10.js
+++ practicum/week-2/question10.js
@@ -8,7 +8,12 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
-    var sorted = Array.from(n.toString()).map(Number);
-    return Math.max(sorted) - Math.min(sorted)
+    var max = n.reduce(function(a, b) {
+        return Math.max(a, b);
+    });
+    var min = n.reduce(function(a, b) {
+        return Math.min(a, b);
+    });
+    return max - min
 
 };
