8bd4fd78ae4b3fe92d00c620036c9eea53fd193e
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-02-15 22:29:08 -0700

    print

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -10,6 +10,16 @@ Example: input: [-12,3,-1,5,-2,1,-7]
           */
 
  var largestSubarray = function(input) {
-     
-    
- };
+        var size = input.length;
+        var max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
+ 
+        for (var i = 0; i < size; i++)
+        {
+            max_ending_here = max_ending_here + a[i];
+            if (max_so_far < max_ending_here)
+                max_so_far = max_ending_here;
+            if (max_ending_here < 0)
+                max_ending_here = 0;
+        }
+        return max_so_far;
+    };
