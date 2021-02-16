cea885bbe29981a7b4ecd0724feee7e47ca2de84
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-02-15 23:00:42 -0700

    Commit

--- practicum/week-3/question3.js
+++ practicum/week-3/question3.js
@@ -13,5 +13,18 @@ output: [
 */
 
 var PascalTriangle = function(n) {
-
+  var arr = [];
+  var tmp;
+  for(var i=0;i<n;i++){
+      arr[i]=[];
+      for(var j=0; j<=i; j++){
+          if(j==i){
+              arr[i].push(1);
+          }else{
+              tmp = (!!arr[i-1][j-1]?arr[i-1][j-1]:0)+(!!arr[i-1][j]?arr[i-1][j]:0);
+              arr[i].push(tmp);
+          }
+      }
+  }
+  return arr;
 };
