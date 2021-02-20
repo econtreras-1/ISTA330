9b9ba4e4450a2f46d5889a84f8e37f7e203782c9
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-02-19 22:42:22 -0700

    Push

--- practicum/week-3/question8.js
+++ practicum/week-3/question8.js
@@ -13,4 +13,25 @@ output: 3
 */
 //Update
 
-var d_integer = function(input) {};
+var d_integer = function(input) {
+    let result = 0;
+    let current = 0;
+    let dint = -1;
+
+    for(let i=0; i < input.length; i++){
+        current = input[i];
+        let count = 0;
+
+        for(let j=0; j < input.length; j++){
+            if(current == input[j]){
+                count++;
+            }
+        }
+
+        if(count == current && count > result){
+            dint = count;
+            result = count;
+        }
+    }
+    return dint;
+};
